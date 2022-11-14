a57953a2ce343a70dfda646aa9115ff82d41b722
memr <memr@itu.dk>
2022-11-14 22:26:34 +0100

    f

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # PeerToPeerAssignment4
+## About
+The program can only be run with three peers.
+
+## How to run the program
 To run the program open a terminal and write the following command: 
 `go run . 0`
 This command will create a peer with port 5000 + 0
 
 Then open two new terminals and write the same command, but with other numbers such as 1 or 2
+
+To make a access to the critical section request write any message in one of the three terminals
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	ping "github.com/Mads0709/PeerToPeerAssignment4.git/grpc"
@@ -26,7 +27,7 @@ type peer struct {
 }
 
 func main() {
-	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32) //svare til flag
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32) //Could also use flags
 	ownPort := int32(arg1) + 5000
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -34,7 +35,6 @@ func main() {
 
 	pe := &peer{
 		id:                           ownPort,
-		amountOfPings:                make(map[int32]int32),
 		clients:                      make(map[int32]ping.PingClient),
 		ctx:                          ctx,
 		timestamp:                    0,
@@ -44,14 +44,14 @@ func main() {
 	// Create listener tcp on port ownPort
 	list, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
 	if err != nil {
-		log.Fatalf("Failed to listen on port: %v", err)
+		log.Fatalf("Failed to listen on port: %v\n", err)
 	}
 	grpcServer := grpc.NewServer()
 	ping.RegisterPingServer(grpcServer, pe)
 
 	go func() {
 		if err := grpcServer.Serve(list); err != nil {
-			log.Fatalf("failed to server %v", err)
+			log.Fatalf("failed to server %v\n", err)
 		}
 	}()
 
@@ -66,7 +66,7 @@ func main() {
 		fmt.Printf("Trying to dial: %v\n", port)
 		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
-			log.Fatalf("Could not connect: %s", err)
+			log.Fatalf("Could not connect: %s\n", err)
 		}
 		defer conn.Close()
 		c := ping.NewPingClient(conn)
@@ -75,61 +75,73 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		pe.sendPingToAll() //Her sender man access requests
+		pe.sendPingToAll() //Here a peer sends an acces to critical section request every time you write in the terminal
 	}
 }
+
+//****************************************************************
+//****************************************************************
+//****************************************************************
 
 func (p *peer) Ping(ctx context.Context, req *ping.Request) (*ping.Reply, error) { //evaluate request
-	id := req.Id
-	p.amountOfPings[id] += 1 //kan slettes
 	p.timestamp += 1
-	//var channel = make(chan bool, 1)
 	fmt.Printf("req.Timestamp: %v , p.timestamp: %v \n", req.Timestamp, p.timestamp)
 
-	for { //Her skal peern evaluere requesten før han giver grønt lys
-		if req.Timestamp < p.timestamp {
-			rep := &ping.Reply{Answer: p.timestamp}
-			return rep, nil
-		}
-		if req.Timestamp == p.timestamp && req.Id < p.id {
-			rep := &ping.Reply{Answer: p.timestamp}
-			return rep, nil
-		}
-		//vænter på at vedkommende i critcal section er færdig
-		// fmt.Printf("id: %v , cannot access critical section", req.Id)
-		// rep := &ping.Reply{Answer: p.timestamp}
-		// return rep, nil
-		//<-channel
+	if req.Timestamp < p.timestamp {
+		rep := &ping.Reply{Answer: p.timestamp, Id: p.id}
+		return rep, nil
 	}
+	if req.Timestamp == p.timestamp && req.Id < p.id {
+		rep := &ping.Reply{Answer: p.timestamp, Id: p.id}
+		return rep, nil
+	}
+
+	wgDefer.Add(1)
+	wgDefer.Wait()
+
+	rep := &ping.Reply{Answer: p.timestamp, Id: p.id}
+	return rep, nil
+
 }
 
-// func (p *peer) Done(ctx context.Context, dn *ping.DoneMessage) (*ping.Reply, error) {
+var wgRequests sync.WaitGroup
+var wgDefer sync.WaitGroup
 
-// }
+func Done(ctx context.Context, dm *ping.DoneMessage) {
+	fmt.Printf("release")
+	wgDefer.Done()
+}
 
 func (p *peer) sendPingToAll() {
 	request := &ping.Request{Id: p.id, Timestamp: p.timestamp}
-	count := 0
-	for id, client := range p.clients {
-		reply, err := client.Ping(p.ctx, request) //Her kaldes ping
-		if err != nil {
-			fmt.Println("something went wrong")
-		}
-		fmt.Printf("Got reply from id: %v timestamp: %v\n", id, reply.Answer)
-		count++
+	for _, client := range p.clients {
+
+		go func() {
+			wgRequests.Add(1)
+			reply, err := client.Ping(p.ctx, request) //Here the ping function is called
+			if err != nil {
+				fmt.Println("something went wrong")
+			}
+			fmt.Printf("Got reply from id: %v timestamp: %v\n", reply.Id, reply.Answer)
+			wgRequests.Done()
+		}()
 		fmt.Printf("My timestamp: %v\n", p.timestamp)
 	}
-	//Her skal man tjekke at man har fået ok fra alle clients
-	if count == len(p.clients) {
-		p.criticalSection()
-		count = 0
-	}
+	wgRequests.Wait()
+	p.criticalSection()
+
 }
 
 func (p *peer) criticalSection() {
 	p.criticalSectionAccessCounter += 1
 	fmt.Printf("id: %v is in the critical section and has a csCounter of: %v\n", p.id, p.criticalSectionAccessCounter)
-	time.Sleep(7 * time.Second) //sleep således at man menneskeligt kan nå at requeste fra de andre terminaler
+	time.Sleep(7 * time.Second)
 	fmt.Printf("id: %v is done\n", p.id)
-	//Her skal peeren signalere til de andre peers at den er færdig med criticalSection
+	
+
+	doneMessage := &ping.DoneMessage{DoneBool: true} //here you signal to the other peers that you are done with the critical section
+	for _, client := range p.clients {
+		client.Done(p.ctx, doneMessage)
+	}
+
 }
